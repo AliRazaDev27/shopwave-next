44cce6290c0b80678eb452530f6a9126857d8a2e
Ali Raza <alirazadev27@gmail.com>
2024-07-19 16:10:00 +0500

    Error: getCategories with cache

--- app/(users)/shop/layout.jsx
+++ app/(users)/shop/layout.jsx
@@ -1,7 +1,9 @@
+import { cache } from "react"
 import { ShopControlls } from "@/components/shop-controlls"
 import { getCategories } from "@/lib/actions"
 export default async function Layout({ children }) {
-  const categories = await getCategories()
+  const _getCategories = cache(async () => await getCategories())
+  const categories = await _getCategories()
   return (
     <>
       <ShopControlls _categories={JSON.stringify(categories)} />
