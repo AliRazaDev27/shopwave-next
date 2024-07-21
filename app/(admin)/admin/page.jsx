c64ce2615588a4683fe9676c556cf62c7b29348d
Ali Raza <alirazadev27@gmail.com>
2024-07-21 10:08:06 +0500

    FEATURE: refactoring to modular code 1

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -1,10 +1,24 @@
+"use client"
 import { Suspense } from "react"
-import { Dashboard } from "@/components/dashboard"
+import { PartOne, PartTwo, PartThree } from "@/components/dashboard";
 
-export default async function Admin() {
+export default function Admin() {
   return (
-    <div>
-      <Dashboard />
-    </div >
+    <div className="grid min-h-screen w-full">
+      <main className="grid flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <Suspense fallback={<div>Loading...</div>}>
+          <PartOne />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <PartTwo />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <PartThree />
+        </Suspense>
+      </main>
+
+    </div>
   );
 }
